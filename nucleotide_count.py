202e826493e5dd84afc2336d0fa4fbaba88f108b
Carline Durmus <bioprogram1316@users.noreply.github.com>
2018-01-16 09:43:45 -0500

    Add files via upload
    This program returns the number of Adenines, Guanines, Cytosines, and Thymines in a DNA sequence.

---
+++ nucleotide_count.py
@@ -0,0 +1,9 @@
+sample = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+print "Number of Adenines: "
+print sample.count("A")
+print "Number of Cytosines: "
+print sample.count("C")
+print "Number of Guanines: "
+print sample.count("G")
+print "Number of Thymines: "
+print sample.count("T")
